a2233370c4b0a13f0ba4becd7bf6c430c6f43d17
MonishPuttu <monishputtu1780@gmail.com>
2026-03-09 12:59:05 +0530

    parser changes

--- parser.py
+++ parser.py
@@ -1,17 +1,55 @@
 import tomllib
 
-def parse_lockfile(lockfile_path):
-    with open(lockfile_path, "rb") as f:
+
+def parse_lockfile(path):
+    with open(path, "rb") as f:
         data = tomllib.load(f)
 
     packages = []
 
+    lock_revision = None
+    if "metadata" in data:
+        lock_revision = data["metadata"].get("revision")
+
     for pkg in data.get("package", []):
+        name = pkg.get("name")
+        version = pkg.get("version")
+
+        wheels = []
+        sdist = None
+
+        if "wheels" in pkg:
+            wheels = pkg["wheels"]
+
+        if "sdist" in pkg:
+            sdist = pkg["sdist"]
+
+        if not wheels and not sdist:
+            source = pkg.get("source", {})
+
+            if "url" in source:
+                sdist = {
+                    "url": source["url"],
+                    "hash": source.get("hash")
+                }
+
+        if "distributions" in pkg:
+            for dist in pkg["distributions"]:
+                if dist.get("type") == "wheel":
+                    wheels.append(dist)
+
+                if dist.get("type") == "sdist":
+                    sdist = dist
+
         packages.append({
-            "name": pkg["name"],
-            "version": pkg["version"],
-            "wheels": pkg.get("wheels", []),
-            "sdist": pkg.get("sdist")
+            "name": name,
+            "version": version,
+            "wheels": wheels,
+            "sdist": sdist,
+            "marker": pkg.get("marker")
         })
 
-    return packages
+    return {
+        "packages": packages,
+        "revision": lock_revision
+    }
