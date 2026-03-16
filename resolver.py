3d5d7dbd9708c755e0c9fd525bf907a764eb9eff
MonishPuttu <monishputtu1780@gmail.com>
2026-03-16 15:35:06 +0530

    sdist first policy similar to pip

--- resolver.py
+++ resolver.py
@@ -1,7 +1,11 @@
 def resolve_artifact(package):
+    sdist = package.get("sdist")
     wheels = package.get("wheels", [])
+
+    if sdist:
+        return sdist
 
     if wheels:
         return wheels[0]
 
-    return package.get("sdist")
+    return None
